849c2acc230f99c75bebd2b68f2b6cae599294cd
minichen <minichen@qq.com>
2016-10-02 22:01:04 +0800

    ..

---
+++ main1/src/main/java/com/my/test/main/main1/Main1.java
@@ -0,0 +1,15 @@
+package com.my.test.main.main1;
+
+import com.my.test.utils.util1.Util1;
+import com.my.test.utils.util2.Util2;
+
+/**
+ * Created by minichen on 16/10/1.
+ */
+public class Main1 {
+    public static void main(String[] args){
+        Util1.sayHello();
+        System.out.print(" ");
+        Util2.sayWorld();
+    }
+}
